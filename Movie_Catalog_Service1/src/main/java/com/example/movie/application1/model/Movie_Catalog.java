f464204427d7daa5b5a0bd1fa2705efa1fd8fff7
lenovo <lenovo@DESKTOP-1USBQSM>
2021-05-11 12:57:56 +0530

    Movie_Catalog_Service1

---
+++ Movie_Catalog_Service1/src/main/java/com/example/movie/application1/model/Movie_Catalog.java
@@ -0,0 +1,75 @@
+package com.example.movie.application1.model;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.ManyToOne;
+
+@Entity
+@IdClass(UserIdMovieIdCompositeKey.class)
+public class Movie_Catalog {
+	
+	@Id
+	private int movieId;
+	@Id
+	private int userId;
+	
+	private String movieName;
+	String opinion;
+	
+	public Movie_Catalog() {
+		super();
+	}
+
+	public Movie_Catalog(int movieId, int userId, String movieName, String opinion) {
+		super();
+		this.movieId = movieId;
+		this.userId = userId;
+		this.movieName = movieName;
+		this.opinion = opinion;
+	}
+
+	public int getMovieId() {
+		return movieId;
+	}
+
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+
+	public String getOpinion() {
+		return opinion;
+	}
+
+	public void setOpinion(String opinion) {
+		this.opinion = opinion;
+	}
+
+	@Override
+	public String toString() {
+		return "Movie_Catalog [movieId=" + movieId + ", userId=" + userId + ", movieName=" + movieName + ", opinion="
+				+ opinion + "]";
+	}
+
+	
+    
+
+}
